618b413cb7258c6bbc7af716bf2b05bab4e79b96
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2022-10-19 13:42:30 +0200

    add validation

--- src/pronunciation_dictionary_utils/words_sorting.py
+++ src/pronunciation_dictionary_utils/words_sorting.py
@@ -2,8 +2,18 @@ from collections import OrderedDict
 
 from pronunciation_dictionary import PronunciationDict
 
+from pronunciation_dictionary_utils.validation import (validate_dictionary, validate_mp_options,
+                                                       validate_type)
+
 
 def sort_words(dictionary: PronunciationDict, descending: bool, consider_case: bool) -> PronunciationDict:
+  if msg := validate_dictionary(dictionary):
+    raise ValueError(f"Parameter 'dictionary': {msg}")
+  if msg := validate_type(descending, bool):
+    raise ValueError(f"Parameter 'descending': {msg}")
+  if msg := validate_type(consider_case, bool):
+    raise ValueError(f"Parameter 'consider_case': {msg}")
+
   words = dictionary.keys()
   sorted_words = sorted(words, reverse=descending,
                         key=lambda w: w if consider_case else (w.lower(), w))
