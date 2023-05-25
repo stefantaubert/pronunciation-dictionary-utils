e7ddbfd26b866c7a280a0064d8444b6215ace849
Natalia Przybysz <ntl.przybysz@gmail.com>
2023-05-25 10:00:51 +0200

    Simplified test_apply_mapping_full and _partial

--- src/pronunciation_dictionary_utils_tests/test_pronunciations_map_symbols_json_py/test_apply_mapping_full.py
+++ src/pronunciation_dictionary_utils_tests/test_pronunciations_map_symbols_json_py/test_apply_mapping_full.py
@@ -1,117 +1,31 @@
 from collections import OrderedDict
-from ordered_set import OrderedSet
 
-from pronunciation_dictionary_utils_cli.pronunciations_map_symbols_json import (
-    apply_mapping_full, get_mappable_symbols)
+from pronunciation_dictionary_utils_cli.pronunciations_map_symbols_json import apply_mapping_full
 
-from pronunciation_dictionary import (MultiprocessingOptions, get_phoneme_set)
+from pronunciation_dictionary import MultiprocessingOptions
 
 
-def test_with_changes_partial_unavailable() -> None:
+def test_with_changes() -> None:
     test_dictionary = OrderedDict([("test", OrderedDict([((("AO2"),), 1)]))])
     mappings = {"AO2": "ˌɔ"}
-
+    mappable_symbol = "AO2"
     expected_result = OrderedDict([("test", OrderedDict([((("ˌɔ"),), 1)]))])
-
-    unique_sounds_in_dictionary = get_phoneme_set(test_dictionary)
-    unique_sounds_in_mappings = set(mappings.keys())
-    mappable_symbols = get_mappable_symbols(unique_sounds_in_dictionary, unique_sounds_in_mappings)
-    sorted_mappable_symbols = OrderedSet(sorted(mappable_symbols, key=len, reverse=True))
-
     mp_options = MultiprocessingOptions(n_jobs=4, maxtasksperchild=100, chunksize=10)
 
-    changed_words_total = set()
-    for mappable_symbol in sorted_mappable_symbols:
-        changed_words = apply_mapping_full(test_dictionary, mappings, mappable_symbol, mp_options)
-        changed_words_total |= changed_words
+    changed_words = apply_mapping_full(test_dictionary, mappings, mappable_symbol, mp_options)
 
-    assert changed_words_total == {"test"}
-    assert test_dictionary == expected_result
-
-
-def test_without_changes_partial_available() -> None:
-    test_dictionary = OrderedDict([("test", OrderedDict([((("AO2"),), 1)]))])
-    mappings = {"AO": "ɔ", "AO2": "ˌɔ"}
-
-    expected_result = OrderedDict([("test", OrderedDict([((("ˌɔ"),), 1)]))])
-
-    unique_sounds_in_dictionary = get_phoneme_set(test_dictionary)
-    unique_sounds_in_mappings = set(mappings.keys())
-    mappable_symbols = get_mappable_symbols(unique_sounds_in_dictionary, unique_sounds_in_mappings)
-    sorted_mappable_symbols = OrderedSet(sorted(mappable_symbols, key=len, reverse=True))
-
-    mp_options = MultiprocessingOptions(n_jobs=4, maxtasksperchild=100, chunksize=10)
-
-    changed_words_total = set()
-    for mappable_symbol in sorted_mappable_symbols:
-        changed_words = apply_mapping_full(test_dictionary, mappings, mappable_symbol, mp_options)
-        changed_words_total |= changed_words
-
-    assert changed_words_total == {"test"}
+    assert changed_words == {"test"}
     assert test_dictionary == expected_result
 
 
 def test_with_whitespaces() -> None:
     test_dictionary = OrderedDict([("test", OrderedDict([((("EY2"),), 1)]))])
     mappings = {"EY2": "ˌe ɪ"}
-
+    mappable_symbol = ("EY2")
     expected_result = OrderedDict([("test", OrderedDict([(("ˌe", "ɪ"), 1)]))])
-
-    unique_sounds_in_dictionary = get_phoneme_set(test_dictionary)
-    unique_sounds_in_mappings = set(mappings.keys())
-    mappable_symbols = get_mappable_symbols(unique_sounds_in_dictionary, unique_sounds_in_mappings)
-    sorted_mappable_symbols = OrderedSet(sorted(mappable_symbols, key=len, reverse=True))
-
     mp_options = MultiprocessingOptions(n_jobs=4, maxtasksperchild=100, chunksize=10)
 
-    changed_words_total = set()
-    for mappable_symbol in sorted_mappable_symbols:
-        changed_words = apply_mapping_full(test_dictionary, mappings, mappable_symbol, mp_options)
-        changed_words_total |= changed_words
+    changed_words = apply_mapping_full(test_dictionary, mappings, mappable_symbol, mp_options)
 
-    assert changed_words_total == {"test"}
-    assert test_dictionary == expected_result
-
-
-def test_without_changes() -> None:
-    test_dictionary = OrderedDict([("test", OrderedDict([((("EY2"),), 1)]))])
-    mappings = {"V": "v"}
-
-    expected_result = OrderedDict([("test", OrderedDict([((("EY2"),), 1)]))])
-
-    unique_sounds_in_dictionary = get_phoneme_set(test_dictionary)
-    unique_sounds_in_mappings = set(mappings.keys())
-    mappable_symbols = get_mappable_symbols(unique_sounds_in_dictionary, unique_sounds_in_mappings)
-    sorted_mappable_symbols = OrderedSet(sorted(mappable_symbols, key=len, reverse=True))
-    
-    mp_options = MultiprocessingOptions(n_jobs=4, maxtasksperchild=100, chunksize=10)
-
-    changed_words_total = set()
-    for mappable_symbol in sorted_mappable_symbols:
-        changed_words = apply_mapping_full(test_dictionary, mappings, mappable_symbol, mp_options)
-        changed_words_total |= changed_words
-
-    assert changed_words_total == set()
-    assert test_dictionary == expected_result
-
-
-def test_empty() -> None:
-    test_dictionary = OrderedDict()
-    mappings = dict()
-
-    expected_result = OrderedDict()
-
-    unique_sounds_in_dictionary = get_phoneme_set(test_dictionary)
-    unique_sounds_in_mappings = set(mappings.keys())
-    mappable_symbols = get_mappable_symbols(unique_sounds_in_dictionary, unique_sounds_in_mappings)
-    sorted_mappable_symbols = OrderedSet(sorted(mappable_symbols, key=len, reverse=True))
-
-    mp_options = MultiprocessingOptions(n_jobs=4, maxtasksperchild=100, chunksize=10)
-
-    changed_words_total = set()
-    for mappable_symbol in sorted_mappable_symbols:
-        changed_words = apply_mapping_full(test_dictionary, mappings, mappable_symbol, mp_options)
-        changed_words_total |= changed_words
-
-    assert changed_words_total == set()
+    assert changed_words == {"test"}
     assert test_dictionary == expected_result
--- src/pronunciation_dictionary_utils_tests/test_pronunciations_map_symbols_json_py/test_apply_mapping_partial.py
+++ src/pronunciation_dictionary_utils_tests/test_pronunciations_map_symbols_json_py/test_apply_mapping_partial.py
@@ -1,115 +1,30 @@
 from collections import OrderedDict
-from ordered_set import OrderedSet
 import pytest
 
-from pronunciation_dictionary_utils_cli.pronunciations_map_symbols_json import (
-    apply_mapping_partial, get_mappable_symbols)
+from pronunciation_dictionary_utils_cli.pronunciations_map_symbols_json import apply_mapping_partial
 
-from pronunciation_dictionary import (MultiprocessingOptions, get_phoneme_set)
+from pronunciation_dictionary import MultiprocessingOptions
 
 
 def test_with_changes() -> None:
-    test_dictionary = OrderedDict([("test", OrderedDict([
-                                    (("AO", "AO2", "AO3", "AA1", "EY2", "."), 1),
-                                    (("A03", "NN", "HH"), 2)
-                                    ]))])
-    mappings = {
-        "EY1": "ˈeɪ",
-        "EY": "eɪ",
-        "AO1": "ˈɔ",
-        "EY0": "eɪ",
-        "AO": "ɔ",
-        "AO0": "ɔ",
-        "AO2": "ˌɔ",
-        "EY2": "ˌeɪ"
-    }
-
-    expected_result = OrderedDict([("test", OrderedDict([
-                                    (("ɔ", "ˌɔ", "ɔ3", "AA1", "ˌeɪ", "."), 1),
-                                    (("A03", "NN", "HH"), 2)]))])
-
-    unique_sounds_in_dictionary = get_phoneme_set(test_dictionary)
-    unique_sounds_in_mappings = set(mappings.keys())
-    mappable_symbols = get_mappable_symbols(unique_sounds_in_dictionary, unique_sounds_in_mappings)
-    sorted_mappable_symbols = OrderedSet(sorted(mappable_symbols, key=len, reverse=True))
-
+    test_dictionary = OrderedDict([("test", OrderedDict([((("AO2"),), 1)]))])
+    mappings = {"AO": "ɔ"}
+    expected_result = OrderedDict([("test", OrderedDict([((("ɔ2"),), 1)]))])
+    mappable_symbol = "AO"
     mp_options = MultiprocessingOptions(n_jobs=4, maxtasksperchild=100, chunksize=10)
 
-    changed_words_total = set()
-    for mappable_symbol in sorted_mappable_symbols:
-        changed_words = apply_mapping_partial(test_dictionary, mappings, mappable_symbol, mp_options)
-        changed_words_total |= changed_words
+    changed_words = apply_mapping_partial(test_dictionary, mappings, mappable_symbol, mp_options)
 
-    assert changed_words_total == {"test"}
+    assert changed_words == {"test"}
     assert test_dictionary == expected_result
 
 
 def test_with_whitespaces() -> None:
     test_dictionary = OrderedDict([("test", OrderedDict([(("EY2",), 1)]))])
     mappings = {"EY2": "ˌe ɪ"}
-
-    unique_sounds_in_dictionary = get_phoneme_set(test_dictionary)
-    unique_sounds_in_mappings = set(mappings.keys())
-    mappable_symbols = get_mappable_symbols(unique_sounds_in_dictionary, unique_sounds_in_mappings)
-    sorted_mappable_symbols = OrderedSet(sorted(mappable_symbols, key=len, reverse=True))
-
+    mappable_symbol = "EY2"
     mp_options = MultiprocessingOptions(n_jobs=4, maxtasksperchild=100, chunksize=10)
 
     with pytest.raises(Exception) as expected_exception:
-        apply_mapping_partial(test_dictionary, mappings, sorted_mappable_symbols[0], mp_options)
+        apply_mapping_partial(test_dictionary, mappings, mappable_symbol, mp_options)
     assert str(expected_exception.value) == "Whitespaces in mappings aren't supported with partial mapping."
-
-
-def test_without_changes() -> None:
-    test_dictionary = OrderedDict([("test", OrderedDict([
-                                    (("AO", "AO2", "AO3", "AA1", "EY2", "."), 1),
-                                    (("A03", "NN", "HH"), 2)
-                                    ]))])
-    mappings = {
-        "V": "v",
-        "F": "f",
-        "D": "d",
-        "MM": "m"
-    }
-
-    expected_result = OrderedDict([("test", OrderedDict([
-                                    (("AO", "AO2", "AO3", "AA1", "EY2", "."), 1),
-                                    (("A03", "NN", "HH"), 2)
-                                    ]))])
-
-    unique_sounds_in_dictionary = get_phoneme_set(test_dictionary)
-    unique_sounds_in_mappings = set(mappings.keys())
-    mappable_symbols = get_mappable_symbols(unique_sounds_in_dictionary, unique_sounds_in_mappings)
-    sorted_mappable_symbols = OrderedSet(sorted(mappable_symbols, key=len, reverse=True))
-
-    mp_options = MultiprocessingOptions(n_jobs=4, maxtasksperchild=100, chunksize=10)
-
-    changed_words_total = set()
-    for mappable_symbol in sorted_mappable_symbols:
-        changed_words = apply_mapping_partial(test_dictionary, mappings, mappable_symbol, mp_options)
-        changed_words_total |= changed_words
-
-    assert changed_words_total == set()
-    assert test_dictionary == expected_result
-
-
-def test_empty() -> None:
-    test_dictionary = OrderedDict()
-    mappings = dict()
-
-    expected_result = OrderedDict()
-
-    unique_sounds_in_dictionary = get_phoneme_set(test_dictionary)
-    unique_sounds_in_mappings = set(mappings.keys())
-    mappable_symbols = get_mappable_symbols(unique_sounds_in_dictionary, unique_sounds_in_mappings)
-    sorted_mappable_symbols = OrderedSet(sorted(mappable_symbols, key=len, reverse=True))
-
-    mp_options = MultiprocessingOptions(n_jobs=4, maxtasksperchild=100, chunksize=10)
-
-    changed_words_total = set()
-    for mappable_symbol in sorted_mappable_symbols:
-        changed_words = apply_mapping_partial(test_dictionary, mappings, mappable_symbol, mp_options)
-        changed_words_total |= changed_words
-
-    assert changed_words_total == set()
-    assert test_dictionary == expected_result
